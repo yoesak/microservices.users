73694b6c55fe3d010c8c655951dd1074a3527355
Yusak Setiawan <zivlab@Yusaks-MacBook-Pro.local>
2018-05-26 23:09:29 +0700

    first commit

---
+++ project/config.py
@@ -0,0 +1,17 @@
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    pass
+
+class TestingConfiguration(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    pass
+
+
